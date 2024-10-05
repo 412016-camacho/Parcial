bcc5608ff77e11ef3dee8240187f6c21c1c19fb7
CamachoGabriela <412016@tecnicatura.frc.utn.edu.ar>
2024-10-05 12:39:42 -0300

    Parcial1

---
+++ Parcial I 2024/CriptomonedasParcial/CriptoBack/Data/Repositories/CriptoRepository.cs
@@ -0,0 +1,49 @@
+﻿using CriptoBack.Data.Entities;
+using CriptoBack.Data.Entities.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriptoBack.Data.Repositories
+{
+    public class CriptoRepository : ICriptoRepository
+    {
+        private readonly CriptomonedasDbContext _context;
+        public CriptoRepository(CriptomonedasDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Delete(int id)
+        {
+            var criptoEliminar = await _context.Criptomonedas.FindAsync(id);
+            if(criptoEliminar != null)
+            {
+                criptoEliminar.Estado = "NH";
+                _context.Criptomonedas.Update(criptoEliminar);
+            }
+            return await _context.SaveChangesAsync()>0;
+        }
+
+        public async Task<List<Criptomoneda>> GetAllByCategory(Categoria categoria)
+        {
+            return await _context.Criptomonedas.Where(x=>x.CategoriaNavigation.Id==categoria.Id).Where(xe=>xe.UltimaActualizacion >= DateTime.Today.AddDays(-1)) .ToListAsync();
+        }
+
+        public async Task<bool> UpdateValue(int id, double valor, string simbolo, DateTime fecha)
+        {
+            var criptoActual = await _context.Criptomonedas.FindAsync(id);
+            var criptoActualizar = await _context.Criptomonedas.Where(x => x.Simbolo == simbolo).ToListAsync();
+            if (valor != null && simbolo != null && criptoActual!= null && fecha!= null)
+            {
+                criptoActual.ValorActual = valor;
+                criptoActual.UltimaActualizacion = fecha;
+                if (criptoActualizar != null)
+                    _context.Criptomonedas.Update(criptoActual);
+            }
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ Parcial I 2024/CriptomonedasParcial/CriptoBack/Services/ICriptoService.cs
@@ -0,0 +1,17 @@
+﻿using CriptoBack.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriptoBack.Services
+{
+    public interface ICriptoService
+    {
+        Task<List<Criptomoneda>> TraerCategorias(Categoria categoria);
+        Task<bool> ActualizarValores(int id, double valor, string simbolo, DateTime fecha);
+        Task<bool> CancelarCripto(int id);
+        bool ValidarFecha(DateTime fecha);
+    }
+}
